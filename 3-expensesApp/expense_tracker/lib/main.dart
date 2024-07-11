6310333aec4dd6ea884c11c1acd53a59d7eea043
Tilek Mukambetov <mkm.tilek@gmail.com>
2024-07-11 16:33:41 +0600

    Section 5: lessons 126-133 (Text Theme, ThemeData in Widget, Dark Mode, Alt Class Constructor, Arr Filtering, Chart Widget)

--- 3-expensesApp/expense_tracker/lib/main.dart
+++ 3-expensesApp/expense_tracker/lib/main.dart
@@ -5,16 +5,59 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
-  runApp(MaterialApp(
-    // theme: ThemeData().copyWith(useMaterial3: true, ... )
-    theme: ThemeData().copyWith(
-        // scaffoldBackgroundColor: Colors.amber,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          foregroundColor: kColorScheme.primaryContainer,
-          backgroundColor: kColorScheme.onPrimaryContainer,
-        )),
-    home: const Expenses(),
-  ));
+  runApp(
+    MaterialApp(
+      // theme: ThemeData().copyWith(useMaterial3: true, ... )
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+          // scaffoldBackgroundColor: Colors.amber,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            foregroundColor: kColorScheme.primaryContainer,
+            backgroundColor: kColorScheme.onPrimaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16,
+                ),
+              )),
+      // themeMode: ThemeMode.system,
+      home: const Expenses(),
+    ),
+  );
 }
